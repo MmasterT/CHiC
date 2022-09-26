ed554d7c8b9c73b56ee361421009fb2b69f409fa
Mariano Olivera Fedi <marianooliverafedi@gmail.com>
2022-09-26 18:10:42 +0100

    updated install.r to install all dependencies

--- install.r
+++ install.r
@@ -6,16 +6,16 @@ options(repos = r)
 
 # install.r is a script for remote package installation. It is used to install packages on remote machines or in image files.
 
-install.packages("argparser", quiet= TRUE, version = "0.7.1")
-install.packages("argparse", quiet= TRUE, version = "2.1.5")
+install.packages("argparser", quiet= TRUE, version = "0.7.1", dependencies=TRUE)
+install.packages("argparse", quiet= TRUE, version = "2.1.5", dependencies=TRUE)
 
-install.packages("plotly", quiet= TRUE, version = "4.10.0")
-install.packages("tidyverse", quiet= TRUE, version = "1.3.2")
+install.packages("plotly", quiet= TRUE, version = "4.10.0", dependencies=TRUE)
+install.packages("tidyverse", quiet= TRUE, version = "1.3.2", dependencies=TRUE)
 
-install.packages("rmarkdown", quiet= TRUE, version = "2.16")
+install.packages("rmarkdown", quiet= TRUE, version = "2.16", dependencies=TRUE)
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager", quiet= TRUE)
+    install.packages("BiocManager", quiet= TRUE, dependencies=TRUE)
 
-BiocManager::install("Chicago", quiet= TRUE)
-BiocManager::install("Rsamtools", quiet= TRUE)
+BiocManager::install("Chicago", quiet= TRUE, dependencies=TRUE)
+BiocManager::install("Rsamtools", quiet= TRUE, dependencies=TRUE)
